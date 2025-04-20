d5108adf0a26456c162d69b895fa24aa6b29d066
Tina Chandwani <tchandwa@usc.edu>
2025-04-19 21:38:42 -0700

    Time: 3 ms (73.35%) | Memory: 17.6 MB (97.78%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,22 +1,14 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        '''
-        Overall Flow is -> you want to reach i step, what is the minimum cost to reach there
-        So Basically , Min (cost to reach i-1 step + cost of i-1 step, cost to reach i-2 step + cost of i-2 step)
-        Base Case -> if step == 0 or 1 return 0 [cause the problem allows us to start at step 0 or 1 without paying any cost]
-        '''
-        def helper(step):
-            if step == 0 or step == 1:
-                return 0
-            
-            if step in memo:
-                minCost = memo[step]
-            else:
-                minCost = min(helper(step-1) + cost[step-1],helper(step-2) + cost[step-2])
-                memo[step] = minCost
 
-            return minCost
-
+        prev1 = 0
+        prev2 = 0
         n = len(cost)
-        memo = {}
-        return helper(n)
+
+        for i in range(2,n+1):
+            current = min(prev1 + cost[i-1], prev2 + cost[i-2])
+
+            prev2 = prev1
+            prev1 = current
+        
+        return prev1
