8270f3cfd653da16f5692199ef52d6f9eb7b56d7
Tina Chandwani <tchandwa@usc.edu>
2025-06-17 00:39:34 -0700

    Time: 7 ms (75.20%) | Memory: 18.2 MB (69.67%) - LeetSync

---
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        commonDict = defaultdict(int)
+        minSum = 999999
+        output = []
+        for i in range(len(list1)):
+            commonDict[list1[i]] = i 
+
+        for i in range(len(list2)):
+            if list2[i] in commonDict:
+                minIndex = i + commonDict[list2[i]]
+                if minIndex < minSum:
+                    minSum = minIndex
+                    output = []
+                    output.append(list2[i])
+                elif minIndex == minSum:
+                    output.append(list2[i])
+        
+        return output
