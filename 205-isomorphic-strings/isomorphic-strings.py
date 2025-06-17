0539444968138c2a0dadeafeae91acabe3bdbb04
Tina Chandwani <tchandwa@usc.edu>
2025-06-16 22:45:14 -0700

    Time: 7 ms (54.63%) | Memory: 18 MB (37.30%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,18 +1,17 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        mpp1 = defaultdict(list)
-        mpp2 = defaultdict(list)
-        leng = len(s)
-
         if len(s) != len(t):
             return False
         
-        for i in range(leng):
-            mpp1[s[i]].append(i)
-            mpp2[t[i]].append(i)
+        charMap = {}
+        n = len(s)
 
-        if len(mpp1) == len(mpp2):
-            if(list(mpp1.values()) == list(mpp2.values())):
-                return True
-        return False
-        
+        for i in range(n):
+            if s[i] in charMap:
+                if charMap[s[i]] != t[i]:
+                    return False
+            elif t[i] in charMap.values():
+                return False
+            charMap[s[i]] = t[i]
+        return True
+            
