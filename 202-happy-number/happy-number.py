b3c2379faa7c3bd4cca5e137b0b2dc5b69802603
Tina Chandwani <tchandwa@usc.edu>
2025-06-17 22:20:13 -0700

    Time: 0 ms (100.00%) | Memory: 17.5 MB (99.79%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,25 +1,20 @@
 class Solution:
-    def happy(self,happyDict,n):
-        happySum = 0
-        for digit in str(n):
-            digit = int(digit)
-            if digit in happyDict:
-                squareDigit = happyDict[digit]
-            else:
-                squareDigit = digit * digit
-            happySum += squareDigit
-        return happySum
+    # def happy(self,happyDict,n):
+    #     happySum = 0
+    #     for digit in str(n):
+    #         digit = int(digit)
+    #         if digit in happyDict:
+    #             squareDigit = happyDict[digit]
+    #         else:
+    #             squareDigit = digit * digit
+    #         happySum += squareDigit
+    #     return happySum
 
     def isHappy(self, n: int) -> bool:
-        happyDict = {}
         seen = set()
+        
         while n != 1 and n not in seen:
             seen.add(n)
-            n = self.happy(happyDict,n)
+            n = sum(int(digit) ** 2 for digit in str(n))
         
-        if n == 1:
-            return True
-        else:
-            return False
-
-        
+        return n == 1
