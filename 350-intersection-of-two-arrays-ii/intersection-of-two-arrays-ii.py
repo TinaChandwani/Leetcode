8948b7100f8222653d65b3d4f395278f6115745e
Tina Chandwani <tchandwa@usc.edu>
2025-06-17 20:51:34 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (94.04%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -2,6 +2,9 @@ class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         countDict = defaultdict(int)
         output = []
+        
+        if len(nums1) > len(nums2):
+            nums1,nums2 = nums2,nums1
         for i in nums1:
             countDict[i] += 1
         
