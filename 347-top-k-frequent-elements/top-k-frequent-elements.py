fe8e5d0b85345155d3d68a116eb5c9970adb33f9
Tina Chandwani <tchandwa@usc.edu>
2025-06-24 18:10:33 -0700

    Time: 15 ms (11.18%) | Memory: 24.1 MB (20.86%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,17 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        topDict = defaultdict(int)
+        answer = []
+        bucket = [[] for _ in range(len(nums) + 1)]
+        
+        for i in nums:
+            topDict[i] += 1
+
+        for j,ke in topDict.items():
+            bucket[ke].append(j)
+        
+        for _ in range(len(nums),0,-1):
+            for num in bucket[_]:
+                answer.append(num)
+                if len(answer) == k:
+                    return answer
