845b598567a4f088563027cdea1258cebe255287
Tina Chandwani <tchandwa@usc.edu>
2025-07-11 15:50:35 -0700

    Time: 3 ms (38.89%) | Memory: 18.6 MB (98.36%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,5 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) == 1:
+            return 0
+        
         n = len(prices) - 1
         i = 0
         max_profit = 0
