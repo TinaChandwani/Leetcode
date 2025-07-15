a3b332bd63b0dedcfc39ad4340c24dd4ce444651
Tina Chandwani <tchandwa@usc.edu>
2025-07-14 19:09:28 -0700

    Time: 32 ms (51.18%) | Memory: 18.8 MB (14.02%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,24 +1,16 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # farthest = 0
-        # n = len(nums) - 1
-        # for i in range(n):
-        #     if i > farthest:
-        #         return False
-        #     farthest = max(farthest,i+nums[i])
-        
-        # if farthest >= n:
-        #     return True
-        # else:
-        #     return False
-
-
-        gas = 0
-        for n in nums:
-            if gas < 0:
+        maxInd = 0
+        n = len(nums) - 1
+        if n == 0:
+            return True
+        for i in range(n):
+            if i > maxInd:
                 return False
-            elif n > gas:
-                gas = n
-            gas -= 1
-            
-        return True
+            maxInd = max(maxInd,i + nums[i])
+        if maxInd >= n :
+            return True
+        else:
+            return False
+        
+        
