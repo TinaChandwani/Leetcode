8eff64a32d959a7fb0de1aa2edb04aa1ccd95837
Tina Chandwani <tchandwa@usc.edu>
2025-08-08 00:41:06 -0700

    Time: 8 ms (29.69%) | Memory: 17.9 MB (44.42%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,12 +1,12 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         intToRomanDict = {1000:"M", 900:"CM", 500:"D",400:"CD",100:"C",90:"XC",50:"L",40:"XL",10:"X",9:"IX",5:"V",4:"IV",1:"I"}
-        answer = ""
+        ans = ""
 
-        for i,symbol in intToRomanDict.items():
-            while i <= num:
-                answer += symbol
-                diff = num - i
+        for n, symbol in intToRomanDict.items():
+            while n <= num:
+                ans += symbol
+                diff = num - n
                 num = diff
-        return answer
+        return ans
             
