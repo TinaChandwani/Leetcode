123ec25a25c63d22b4da4fa7d336aa3783c0eb10
Tina Chandwani <tchandwa@usc.edu>
2025-08-14 15:49:12 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (65.66%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -10,12 +10,8 @@ class Solution:
             return True
         if not p or not q or p.val != q.val:
             return False
-        ans_left = self.isSameTree(p.left,q.left)
-        ans_right = self.isSameTree(p.right,q.right)
-
-        if ans_left and ans_right:
-            return True
-        else:
-            return False
-
         
+        l = self.isSameTree(p.left,q.left)
+        r = self.isSameTree(p.right,q.right)
+
+        return (l and r)
