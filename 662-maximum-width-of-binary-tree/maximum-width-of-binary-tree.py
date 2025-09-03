b75fbab8d9e9c0eddb857548693821321310cddf
Tina Chandwani <tchandwa@usc.edu>
2025-09-02 19:51:30 -0700

    Time: 4 ms (45.74%) | Memory: 18.7 MB (33.53%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
@@ -22,10 +22,10 @@ class Solution:
                 if i == lenQ-1:
                     last = curr_index
                 if curr_node.left:
-                    print("Curr->Val: ", curr_node.val, "Index: ",(curr_index-base)*2 + 1)
+                    # print("Curr->Val: ", curr_node.val, "Index: ",(curr_index-base)*2 + 1)
                     q.append((curr_node.left, curr_index*2 + 1))
                 if curr_node.right:
-                    print("Curr->Val: ", curr_node.val, "Index: ",(curr_index-base)*2 + 2)
+                    # print("Curr->Val: ", curr_node.val, "Index: ",(curr_index-base)*2 + 2)
                     q.append((curr_node.right, curr_index*2 + 2))
                 maxWidth = max(maxWidth, last-first+1)
         
