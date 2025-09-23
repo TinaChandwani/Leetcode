a117ac0f52d14bd09daf04b93c4cb505b8861fbc
Tina Chandwani <tchandwa@usc.edu>
2025-09-23 15:19:45 -0700

    Time: 2 ms (68.93%) | Memory: 17.8 MB (83.22%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/repeated-substring-pattern.py
@@ -1,23 +1,6 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        # Applying KMP algorithm
-        m = len(s)
-        i = 1
-        prev = 0
-        lps = [0] * m
-        while i < m:
-            if s[i] == s[prev]:
-                lps[i] = prev + 1
-                prev += 1
-                i += 1
-            else:
-                if prev == 0:
-                    lps[i] = 0
-                    i += 1
-                else:
-                    prev = lps[prev-1]
-        k = lps[-1]
-        if k > 0:
-            if m % (m-k) == 0:
-                return True
+        con = (s + s)[1:-1]
+        if s in con:
+            return True
         return False
