46bfe775aefccc45f1acf99b3a6df07ba9a0ed6b
Tina Chandwani <tchandwa@usc.edu>
2025-09-22 22:21:27 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (32.49%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,44 +1,19 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        '''
-        KMP Algorithm -> O(m+n) Time Complexity
-        The two phases/steps to follow :
-        1. Create a LPS array (longest prefix suffix array) using pattern (needle)
-        2. Using the LPS array solve the problem in linear time
-        '''
+        # Naive Approach
         m = len(needle)
         n = len(haystack)
-        if m == "":
+        if m == 0 or n == 0:
             return 0
         if m > n:
             return -1
-        # LPS Array
-        i = 1
-        lps = [0] * m
-        prev = 0
-        while i < m:
-            if needle[i] == needle[prev]:
-                lps[i] = prev + 1
-                prev += 1
-                i += 1
-            elif prev == 0:
-                lps[i] = 0
-                i += 1
-            else:
-                prev = lps[prev - 1]
-        
-        # Linearly
-        i = 0
-        j = 0
-        while i < n:
-            if needle[j] == haystack[i]:
-                i += 1
-                j += 1
-            else:
-                if j == 0:
-                    i += 1
-                else:
-                    j = lps[j-1]
-            if j == m:
-                return i - m
+        for i in range(n):
+            if needle[0] == haystack[i]:
+                j = i
+                while j < n and j < (i + m) and haystack[j] == needle[j-i]:
+                    j += 1
+                if j-i == m:
+                    return i
         return -1
+
+        
