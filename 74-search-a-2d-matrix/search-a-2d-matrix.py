2b2b81fefa92989ce6c4f1d4bfc40e452ab0c948
Tina Chandwani <tchandwa@usc.edu>
2025-10-06 19:27:25 -0700

    Time: 0 ms (100.00%) | Memory: 18.2 MB (72.23%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,22 +1,38 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        '''
+        Basically the approach is :
+        1. Do a binary search for the rows
+        2. Do a binary search in that row
+        '''
         if not matrix or not matrix[0]:
             return False
-        m = len(matrix) #rows
-        n = len(matrix[0]) # cols
-        total = m * n
-        l = 0
-        r = total - 1
-        while r >= l:
-            m = (l+r)//2
-            row = m // n
-            col = m % n
-            val = matrix[row][col]
-            if val == target:
-                return True
-            elif target < val:
-                r = m - 1
+        n = len(matrix) # rows
+        m = len(matrix[0]) #cols
+        gl = 0
+        gr = n - 1
+        res_row = -1
+        def bs(l,r,row):
+            while r >= l:
+                m = (l + r) // 2
+                if target == matrix[row][m]:
+                    return True
+                elif target < matrix[row][m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            return False
+            
+        while gr >= gl:
+            mr = (gl + gr) // 2
+            if matrix[mr][0] <= target <= matrix[mr][m-1]:
+                res_row = mr
+                break
+            elif matrix[mr][0] < target:
+                gl = mr + 1
             else:
-                l = m + 1
-        return False
+                gr = mr - 1
         
+        return res_row!=-1 and bs(0,m-1,res_row)
+
+       
