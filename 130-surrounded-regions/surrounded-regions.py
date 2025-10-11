9e2af424e99a646556b218f1ba12f14a0f1bff46
Tina Chandwani <tchandwa@usc.edu>
2025-10-10 21:21:36 -0700

    Time: 11 ms (28.03%) | Memory: 22.1 MB (26.06%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.py
@@ -0,0 +1,37 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        q = deque()
+        m,n =  len(board),len(board[0])
+        visited = [[0 for _ in range(n)] for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == "O":
+                    visited[i][j] = 2
+                    if i == 0 or i == m-1 or j==0 or j== n-1:
+                        visited[i][j] = 1
+                        q.append((i,j))
+                else:
+                    visited[i][j] = 3
+        
+        while q:
+            r,c = q.pop()
+            for row,col in [(r,c+1),(r+1,c),(r,c-1),(r-1,c)]:
+                if 0<=row<m and 0<=col<n:
+                    if visited[row][col] == 0 or visited[row][col] == 2 and board[row][col] == "O" :
+                        visited[row][col] = 1
+                        q.append((row,col))
+        
+        for i in range(m):
+            for j in range(n):
+                if visited[i][j] == 2:
+                    board[i][j] = "X"
+
+
+                
+
+                
+        
