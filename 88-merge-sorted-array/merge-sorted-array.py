81e2847f06928b1286d4d0994c06155a037d4df1
Tina Chandwani <tchandwa@usc.edu>
2025-11-07 03:08:15 -0800

    Time: 0 ms (100.00%) | Memory: 18.1 MB (25.69%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,15 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        if m == 0 :
+            nums1[:] = list(nums2)
+            return
+
         p1 = m - 1
         p2 = n - 1
-        p = m + n - 1
+        p = m + n -1
 
-        while p2 >= 0:
-            if p1 >= 0 and nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
-            else:
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] <= nums2[p2]:
                 nums1[p] = nums2[p2]
                 p2 -= 1
+            elif nums1[p1] > nums2[p2]:
+                nums1[p],nums1[p1] = nums1[p1],nums1[p]
+                p1 -= 1 
+            p -= 1
+        while p2 >= 0 :
+            nums1[p] = nums2[p2]
+            p2 -= 1
             p -= 1
