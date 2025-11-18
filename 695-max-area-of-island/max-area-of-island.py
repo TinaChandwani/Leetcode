3c47aed754e32761b064678b7c27dc317b435e9a
Tina Chandwani <tchandwa@usc.edu>
2025-11-18 03:21:59 -0800

    Time: 34 ms (23.42%) | Memory: 18.2 MB (66.46%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.py
@@ -0,0 +1,31 @@
+from collections import deque
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        q = deque()
+        m = len(grid) # rows
+        n = len(grid[0]) # cols
+        visit = set()
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        max_count = 0
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 :
+                    q.append((i,j))
+                    count = 1
+                    while q:
+                        r,c = q.popleft()
+                        visit.add((r,c))
+                        for row,col in directions:
+                            nr = r + row
+                            nc = c + col
+                            if 0 <= nr < m and 0 <= nc < n and (nr,nc) not in visit and grid[nr][nc] == 1:
+                                q.append((nr,nc))
+                                visit.add((nr,nc))
+                                count += 1
+                    max_count = max(max_count,count)
+        
+        return max_count
+                                
+                            
+
