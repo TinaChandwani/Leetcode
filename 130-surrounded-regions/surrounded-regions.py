dd5475c7a20679551c9a0bb0c14e0493718b58f3
Tina Chandwani <tchandwa@usc.edu>
2025-11-24 21:13:37 -0800

    Time: 11 ms (28.50%) | Memory: 21.9 MB (30.23%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,35 +3,30 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        m = len(board) # rows
+        n = len(board[0]) # cols
         q = deque()
-        m,n =  len(board),len(board[0])
-        visited = [[0 for _ in range(n)] for _ in range(m)]
+        visited = [[0 for _ in range(n)] for j in range(m)]
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
 
         for i in range(m):
             for j in range(n):
-                if board[i][j] == "O":
-                    visited[i][j] = 2
-                    if i == 0 or i == m-1 or j==0 or j== n-1:
-                        visited[i][j] = 1
-                        q.append((i,j))
-                else:
-                    visited[i][j] = 3
+                if board[i][j] == 'O' and (i == 0 or j == 0 or i == m-1 or j == n-1):
+                    q.append((i,j))
+                    visited[i][j] = 1
+ 
         
         while q:
-            r,c = q.pop()
-            for row,col in [(r,c+1),(r+1,c),(r,c-1),(r-1,c)]:
-                if 0<=row<m and 0<=col<n:
-                    if visited[row][col] == 2 and board[row][col] == "O" :
-                        visited[row][col] = 1
-                        q.append((row,col))
+            r,c = q.popleft()
+            for row,col in directions:
+                nr = row + r
+                nc = col + c
+                if 0 <= nr < m and 0 <= nc < n and board[nr][nc] == 'O' and visited[nr][nc] == 0:
+                    visited[nr][nc] = 1
+                    q.append((nr,nc))
         
         for i in range(m):
             for j in range(n):
-                if visited[i][j] == 2:
-                    board[i][j] = "X"
-
-
-                
-
-                
-        
+                if board[i][j] == 'O' and visited[i][j] == 0:
+                    board[i][j] = 'X'
+      
