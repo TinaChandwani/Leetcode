5adfd9e0229ecf17dccd469a079e8ebf2bc2731c
Tina Chandwani <tchandwa@usc.edu>
2025-11-26 16:54:33 -0800

    Time: 15 ms (7.95%) | Memory: 18.8 MB (98.18%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,35 +1,34 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        '''
-        Using Kahn Algorithm BFS
-        Use the In degree concept
-        '''
-        indegree = [0] * numCourses
-        visit = set()
+        # Create an adj list
+        # I need to reset inRecursion call after every node finishes
         adj = defaultdict(list)
-        # Create Adj and populate the indegree array
-        for i, j in prerequisites:
+        for i,j in prerequisites:
             adj[i].append(j)
-            indegree[j] += 1
+        visit = set()
         for i in range(numCourses):
             if i in visit:
                 continue
             q = deque()
-
-            for d in range(len(indegree)):
-                if indegree[d] == 0 and d not in visit:
-                    q.append(d)
+            inRecursion = [False] * numCourses
+            q.append((i,True)) # node, enter state
             while q:
-                node = q.popleft()
-                visit.add(node)
-                for n in adj[node]:
-                    indegree[n] -= 1
-                    if indegree[n] == 0:
-                        q.append(n)
-        for v in range(numCourses):
-            if v not in visit:
-                return False
+                node,enter = q.pop()
+                if enter:
+                    visit.add(node)
+                    if inRecursion[node]:
+                        return False
+                    inRecursion[node] = True
+                    q.append((node,False))
+                    for j in adj[node]:
+                        if j not in visit:
+                            q.append((j,True))
+                        elif inRecursion[j] == True:
+                            return False   
+                else:
+                    inRecursion[node] = False
         return True
-                
+
 
         
+         
