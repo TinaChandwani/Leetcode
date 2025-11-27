6f23664048c22c695ce42784390afae8ce616bc4
Tina Chandwani <tchandwa@usc.edu>
2025-11-26 16:45:13 -0800

    Time: 107 ms (5.00%) | Memory: 19.1 MB (72.05%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,34 +1,35 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # Create an adj list
-        # I need to reset inRecursion call after every node finishes
-        adj = defaultdict(list)
-        for i,j in prerequisites:
-            adj[i].append(j)
+        '''
+        Using Kahn Algorithm BFS
+        Use the In degree concept
+        '''
+        indegree = [0] * numCourses
         visit = set()
+        adj = defaultdict(list)
+        # Create Adj and populate the indegree array
+        for i, j in prerequisites:
+            adj[i].append(j)
+            indegree[j] += 1
         for i in range(numCourses):
             if i in visit:
                 continue
             q = deque()
-            inRecursion = [False] * numCourses
-            q.append((i,True)) # node, enter state
-            while q:
-                node,enter = q.pop()
-                if enter:
-                    visit.add(node)
-                    if inRecursion[node]:
-                        return False
-                    inRecursion[node] = True
-                    q.append((node,False))
-                    for j in adj[node]:
-                        if j not in visit:
-                            q.append((j,True))
-                        elif inRecursion[j] == True:
-                            return False   
-                else:
-                      inRecursion[node] = False
-        return True
 
+            for d in range(len(indegree)):
+                if indegree[d] == 0 and d not in visit:
+                    q.append(d)
+            while q:
+                node = q.popleft()
+                visit.add(node)
+                for n in adj[node]:
+                    indegree[n] -= 1
+                    if indegree[n] == 0:
+                        q.append(n)
+        for v in range(numCourses):
+            if v not in visit:
+                return False
+        return True
+                
 
         
-         
