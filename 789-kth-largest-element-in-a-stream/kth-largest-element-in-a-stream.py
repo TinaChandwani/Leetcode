03a181da6acf38b9ca2caa19f68c0f8d4fcfd4f8
Tina Chandwani <tchandwa@usc.edu>
2026-01-13 22:59:10 -0800

    Time: 20 ms (40.53%) | Memory: 25.9 MB (6.98%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -1,25 +1,36 @@
 import heapq
 class KthLargest:
+    '''
+    Heap :
+    log(N) => Insertions and deletions
+    It is complete binary tree
+    To build a heap => O(N)
+
+    For this problem the complexity is :
+    Insertions => Mlog(k) where M is number of add calls
+    Deletions => Nlog(k) where N is len(nums) 
+    '''
 
     def __init__(self, k: int, nums: List[int]):
-        # Basically add in the heap here do not let it go more than k
+        # Create a heap with size = k
         self.heap = []
         self.k = k
+
         for i in nums:
             heapq.heappush(self.heap,i)
-            if len(self.heap) > self.k:
+            if len(self.heap) > k:
                 heapq.heappop(self.heap)
 
-        
-
     def add(self, val: int) -> int:
-        # This addds the new score and returns the highest element always
         if len(self.heap) < self.k:
             heapq.heappush(self.heap,val)
-        elif val > self.heap[0]:
+        elif val > self.heap[0] :
             heapq.heappop(self.heap)
             heapq.heappush(self.heap,val)
         return self.heap[0]
+
+        
+
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
