fd1b953db71035271f6d93edb23554f532b850f7
Tina Chandwani <tchandwa@usc.edu>
2026-01-18 03:13:31 -0800

    Time: 31 ms (80.18%) | Memory: 23.1 MB (29.23%) - LeetSync

---
+++ 817-design-hashmap/design-hashmap.py
@@ -0,0 +1,45 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.buckets = 1009
+        self.hashMap = [[] for j in range(self.buckets)]
+    
+    def getKey(self,key: int):
+        return key % self.buckets
+
+    def put(self, key: int, value: int) -> None:
+        index = self.getKey(key)
+        present = False
+        for k in range(len(self.hashMap[index])):
+            if self.hashMap[index][k][0] == key:
+                present = True
+                self.hashMap[index][k][1] = value
+                return
+        if not present:
+            self.hashMap[index].append([key,value])
+
+    def get(self, key: int) -> int:
+        index = self.getKey(key)
+        for k,v in self.hashMap[index]:
+            if k == key:
+                print(self.hashMap[index])
+                return v
+        return -1
+        
+
+    def remove(self, key: int) -> None:
+        index = self.getKey(key)
+        if len(self.hashMap[index]) > 0:
+            for i in range(len(self.hashMap[index])):
+                if self.hashMap[index][i][0] == key:
+                    del self.hashMap[index][i]
+                    return
+                
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
