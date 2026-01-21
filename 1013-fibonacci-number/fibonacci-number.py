dde1e5d22c8b74c9e991daadb74d69dd99f4ad45
Tina Chandwani <tchandwa@usc.edu>
2026-01-21 02:16:23 -0800

    Time: 47 ms (43.29%) | Memory: 19.2 MB (16.49%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,13 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
         '''
-        Bottom Up solution DP
+        Constant Space Complexity
         '''
         if n <= 1:
             return n
-        dp = [-1] * (n+1)
-        dp[0] = 0
-        dp[1] = 1
-        for i in range(2,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n] 
+        a = 0
+        b = 1
+        for i in range(1,n):
+            a,b = b,a+b
+        return b
+        
