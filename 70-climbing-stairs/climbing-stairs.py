a80a3fdc5df38e33d233ad8eab58f8447af8ad75
Tina Chandwani <tchandwa@usc.edu>
2026-01-21 14:25:24 -0800

    Time: 0 ms (100.00%) | Memory: 19.4 MB (15.64%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,12 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         '''
-        Bottom up Approach
+        This is similar to Fibonacci series.
+        i ka nikalne ke lie, you just need 1 pehle and 2 pehle
         '''
         if n <= 2:
             return n
-        dp = [0] * (n+1)
-        dp[0],dp[1],dp[2] = 0,1,2
+        a,b = 1,2
         for i in range(3,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n]
+            a,b = b,a+b
+        return b
