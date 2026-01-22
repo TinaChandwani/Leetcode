f4cf0b55e0ad2ecd761330900b47dc65bf18ac37
Tina Chandwani <tchandwa@usc.edu>
2026-01-22 00:25:11 -0800

    Time: 0 ms (100.00%) | Memory: 19.4 MB (7.47%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,15 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         '''
-        Bottom Up Approach
+        Constant Space complexity 
         '''
         n = len(nums)
-        if n == 0:
-            return 0
-        dp = [0] * (n+1) 
-        dp[1] = nums[0]
+        if n == 1:
+            return nums[0]
+        prevPrev = 0
+        prev = nums[0]
         for i in range(2,n+1):
-            steal = nums[i-1] + dp[i-2]
-            skip = dp[i-1]
-            dp[i] = max(steal,skip)
-        return dp[n]
+            steal = nums[i-1] + prevPrev
+            skip = prev
+            temp = max(steal,skip)
+            prevPrev = prev
+            prev = temp
+        return prev
