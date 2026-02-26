fcff19707d86f18872b88edb4b5b5517b286e8fd
Tina Chandwani <tchandwa@usc.edu>
2026-02-25 19:42:00 -0800

    Time: 3 ms (76.49%) | Memory: 23.6 MB (32.16%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -1,17 +1,16 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        if bills[0] in (10,20):
-            return False
         fives = 0
         tens = 0
         for i in bills:
             if i == 5:
                 fives += 1
             elif i == 10:
-                if fives == 0:
+                tens +=1 
+                if fives > 0:
+                    fives -= 1
+                else:
                     return False
-                fives -= 1
-                tens += 1
             else:
                 if fives > 0 and tens > 0:
                     fives -= 1
@@ -19,5 +18,6 @@ class Solution:
                 elif fives >= 3:
                     fives -= 3
                 else:
-                    return False     
+                    return False
         return True
+        
