732421574f2394d0eec08ec3eae8fcd6dad4bdb9
Tina Chandwani <tchandwa@usc.edu>
2026-03-16 01:48:43 -0700

    Time: 120 ms (23.90%) | Memory: 24.4 MB (10.90%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
@@ -11,23 +11,21 @@ class Solution:
                 p = s1.pop()
                 nsr[p] = i - p
             s1.append(i)
-        while s1:
-            p = s1.pop()
-            nsr[p] = n - p  
         
         # NSL
         s2 = []
-        nsl = [0] * n
-        for j in range(n):
-            while s2 and arr[s2[-1]] >= arr[j]:
-                s2.pop()
-            nsl[j] = j - s2[-1] if s2 else j + 1
+        nsl = [-1] * n
+        for j in range(n-1,-1,-1):
+            while s2 and arr[s2[-1]] > arr[j]:
+                p = s2.pop()
+                nsl[p] = p - j
+            # nsl[j] = j - s2[-1] if s2 else j + 1
             s2.append(j)
         print(f'nsr {nsr}')
         print(f'nsl {nsl}')
         for k in range(n):
-            l = nsr[k]
-            r = nsl[k] 
+            l = nsr[k] if nsr[k] != n else n - k 
+            r = nsl[k] if nsl[k] != -1 else k + 1
             total = l * r * arr[k]
             ans += total
 
