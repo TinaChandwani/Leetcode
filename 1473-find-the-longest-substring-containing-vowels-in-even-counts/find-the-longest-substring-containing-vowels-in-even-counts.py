4fbfb0ce15acb143fba31e3bd7b0c0f03f3e105a
Tina Chandwani <tchandwa@usc.edu>
2026-03-21 03:09:03 -0700

    Time: 242 ms (31.41%) | Memory: 22 MB (86.22%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -1,26 +1,25 @@
 class Solution:
     def findTheLongestSubstring(self, s: str) -> int:
         vDict = {}
-        vDict['00000'] = -1
+        vDict[0] = -1
         result = 0
-        vowels = [0] * 5 #[a,e,i,o,u]
+        mask = 0
 
         for i in range(len(s)):
             if s[i] == 'a':
-                vowels[0] = vowels[0] ^ 1
+                mask = mask ^(1 << 0)
             elif s[i] == 'e':
-                vowels[1] = vowels[1] ^ 1
+                mask = mask ^(1 << 1)
             elif s[i] == 'i':
-                vowels[2] = vowels[2] ^ 1
+                mask = mask ^(1 << 2)
             elif s[i] == 'o':
-                vowels[3] = vowels[3] ^ 1
+                mask = mask ^(1 << 3)
             elif s[i] == 'u':
-                vowels[4] = vowels[4] ^ 1
+                mask = mask ^(1 << 4)
             
-            v = ''.join(map(str,vowels))
-            if v in vDict:
-                result = max(result,i - vDict[v])
+            if mask in vDict:
+                result = max(result,i - vDict[mask])
             else:
-                vDict[v] = i
+                vDict[mask] = i
 
         return result
