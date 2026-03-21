20278878a9913f741fc528db346a8536fcbf3cbe
Tina Chandwani <tchandwa@usc.edu>
2026-03-21 03:03:44 -0700

    Time: 986 ms (8.34%) | Memory: 22.1 MB (86.22%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -7,15 +7,15 @@ class Solution:
 
         for i in range(len(s)):
             if s[i] == 'a':
-                vowels[0] = (vowels[0] + 1) % 2
+                vowels[0] = vowels[0] ^ 1
             elif s[i] == 'e':
-                vowels[1] = (vowels[1] + 1) % 2
+                vowels[1] = vowels[1] ^ 1
             elif s[i] == 'i':
-                vowels[2] = (vowels[2] + 1) % 2
+                vowels[2] = vowels[2] ^ 1
             elif s[i] == 'o':
-                vowels[3] = (vowels[3] + 1) % 2
+                vowels[3] = vowels[3] ^ 1
             elif s[i] == 'u':
-                vowels[4] = (vowels[4] + 1) % 2
+                vowels[4] = vowels[4] ^ 1
             
             v = ''.join(map(str,vowels))
             if v in vDict:
