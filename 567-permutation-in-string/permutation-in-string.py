dccc0338205a4b45daa652d7bc7b132815bea16a
Tina Chandwani <tchandwa@usc.edu>
2026-03-24 18:26:16 -0700

    Time: 21 ms (50.31%) | Memory: 19.2 MB (93.87%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,34 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        l = 0
+        r = 0
+        n = len(s2)
+        s1Dict = {}
+        s2Dict = defaultdict(int)
+
+        for i in s1:
+            if i in s1Dict:
+                s1Dict[i] += 1
+            else:
+                s1Dict[i] = 1
+
+        while r < n:
+            if s2[r] in s1Dict:
+                s2Dict[s2[r]] += 1
+            
+            while r - l + 1 > len(s1):
+                # Shrink
+                if s2[l] in s1Dict:
+                    s2Dict[s2[l]] -=1
+                    if s2Dict[s2[l]] == 0:
+                        del s2Dict[s2[l]]
+                l += 1
+            if s1Dict == s2Dict:
+                return True
+            
+            r += 1
+        
+        return False
