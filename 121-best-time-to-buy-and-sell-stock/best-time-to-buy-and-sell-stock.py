7c6cd7271aa4633d6fb3dc474b5529d8456842f2
Tina Chandwani <tchandwa@usc.edu>
2026-03-27 03:21:00 -0700

    Time: 67 ms (21.55%) | Memory: 28.6 MB (59.18%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        stack = []
-        r = 0
-        n = len(prices)
         diff = 0
-
-        while r < n:
-            while stack and stack[-1] > prices[r]:
-                stack.pop()
-            if not stack:
-                stack.append(prices[r])
-            else:
-                diff = max(diff, prices[r] - stack[-1])
-            r += 1
-        
+        min_price = float('inf')
+        for i in range(len(prices)):
+            min_price = min(min_price,prices[i])
+            diff = max(diff,prices[i] - min_price)
         return diff
