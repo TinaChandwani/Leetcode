eae9926b5f84ab7ed200da6334e393cd6239fe39
Tina Chandwani <tchandwa@usc.edu>
2026-03-29 15:40:23 -0700

    Time: 0 ms (100.00%) | Memory: 20.6 MB (43.05%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,32 +1,27 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # Iterative
         l = 0
-        n = len(nums)
-        r = n - 1
-        ans = []
-        if n == 1 and target == nums[0]:
-            return [0,0]
-        while r >= l:
-            m = (l + r) // 2
-            print(f'm = {m}')
-            if target == nums[m]:
-                print('found')
-                first = m
-                last = m
-                while m > 0 and nums[m-1] == target :
-                    print(f'm-1 = {m-1}')
-                    first = m - 1
-                    m = m - 1
-                while m < (n-1) and nums[m+1] == target :
-                    print(f'm+1 = {m+1}')
-                    last = m + 1
-                    m = m + 1
-                print(f'ans {[first,last]}')
-                return [first,last]
-            if target > nums[m]:
-                l = m + 1
-            else:
-                r = m - 1
-        return [-1,-1]
+        r = len(nums) - 1
 
+        def left_most(idx): 
+            while idx > 0 and nums[idx - 1] == target:
+                idx -= 1
+            return idx
+        
+        def right_most(idx):
+            while idx < len(nums) - 1 and nums[idx + 1] == target:
+                idx += 1
+            return idx
+        
+        while l <= r:
+            m = l + (r-l) // 2
+            if nums[m] == target:
+                i = left_most(m)
+                j = right_most(m)
+                return [i,j]
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                l = m + 1
+
+        return [-1,-1]
