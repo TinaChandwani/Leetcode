c85dd84a3a6c9326b71b327506fe67d67a6d1127
Tina Chandwani <tchandwa@usc.edu>
2026-03-29 15:54:21 -0700

    Time: 0 ms (100.00%) | Memory: 20.7 MB (43.05%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,27 +1,27 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        l = 0
-        r = len(nums) - 1
-
-        def left_most(idx): 
-            while idx > 0 and nums[idx - 1] == target:
-                idx -= 1
-            return idx
+        def left_most(l,r):
+            while l <= r:
+                m = l + (r-l) // 2
+                if nums[m] >= target:
+                    r = m - 1
+                elif nums[m] < target:
+                    l = m + 1
+            return l
         
-        def right_most(idx):
-            while idx < len(nums) - 1 and nums[idx + 1] == target:
-                idx += 1
-            return idx
+        def right_most(l,r):
+            while l <= r:
+                m = (l + r) // 2
+                if nums[m] <= target:
+                    l = m + 1
+                elif nums[m] > target:
+                    r = m - 1
+            return r
         
-        while l <= r:
-            m = l + (r-l) // 2
-            if nums[m] == target:
-                i = left_most(m)
-                j = right_most(m)
-                return [i,j]
-            elif nums[m] > target:
-                r = m - 1
-            else:
-                l = m + 1
+        i = left_most(0,len(nums)-1)
+        j = right_most(0,len(nums)-1)
+        
+        if i <= j and i < len(nums) and nums[i] == target:
+            return [i,j]
 
         return [-1,-1]
