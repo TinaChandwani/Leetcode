7dbad820f790f0df7d4cc4b85f41f34eba163a0c
Tina Chandwani <tchandwa@usc.edu>
2026-04-22 20:35:30 -0700

    Time: 4 ms (29.83%) | Memory: 19.7 MB (18.74%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,59 +1,50 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        '''
-        Basically to find median of two sorted array meaning to decide where to cut in both the arrays so we get the median
-        1) First think of about the condition where the cut will either land on the median or the median is after the cut (odd or even for both it should be the condition) => T = (m+n) then => L = (T + 1) // 2
-        2) so in order to get this boundary if we take i elemets from left then how many elements from right should we take => (L-i)
-        3) Then think about the boundaries L1,L2,R1,R2:
-            So L1 is the last element in nums1
-            R1 - first element in nums1
-            L2 - last element in nums2
-            R2- first element in nums2
-        4) Think of the inequalities between these boundaries 
-        ===> L1 <= R2 and L2 <= R1
-        5) If L1>R2 ==> then we move left
-        6) If L2 > R1 ==> we move right
-        7) Do a binary search on smaller array, so if we take m from nums1 then we will take T-m elements from nums2. then check for condition if L1 > R2 yes then move left else move right, if nothing of that is the case then return
-        '''
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2,nums1)
+        
+        m = len(nums1)
+        n = len(nums2)
 
-        if len(nums1) >= len(nums2) :
-            nums1,nums2 = nums2,nums1
-        n, m = len(nums1), len(nums2)
-        t = m + n
         l = 0
-        r = n
-        L = (t+1) // 2
-        while r >= l:
-            mid = (l+r)//2
-            j = L - mid
-            if mid > 0:
-                l1 = nums1[mid-1]
-            else:
-                l1 = float('-inf')
-            if mid < n:
-                r1 = nums1[mid]
-            else:
-                r1 = float('inf')
-            if j > 0:
-                l2 = nums2[j-1]
-            else:
-                l2 = float('-inf')
-            if j < m:
-                r2 = nums2[j]
-            else:
-                r2 = float('inf')
-            
-            if l1 > r2:
-                # Shift left
-                r = mid - 1
-            elif l2 > r1:
-                # Shift Right
-                l = mid + 1
-            else:
-                if t % 2 == 0:
-                    return (max(l1,l2) + min(r1,r2)) / 2.0
-                else:
-                    return float(max(l1,l2))
-        return -1
+        r = m
 
+        while l <= r:
+            Px = l + (r-l)// 2 # mid
+            Py = ((m+n+1) // 2) - Px
+
+            # Left wale
+            if Px == 0:
+                x1 = float('-inf')
+            else:
+                x1 = nums1[Px-1]
+
+            if Py == 0:    
+                x2 = float('-inf')
+            else: 
+                x2 = nums2[Py-1]
             
+            # Right wale
+            if Px == m:
+                x3 = float('inf')
+            else:
+                x3 = nums1[Px]
+            
+            if Py == n:
+                x4 = float('inf')
+            else:
+                x4 = nums2[Py]    
+            
+            if x1 <= x4 and x2 <= x3:
+                if (m+n) % 2 == 0:
+                    return (max(x1,x2) + min(x3,x4)) / 2.0
+                else:
+                    return max(x1,x2)
+            
+            if x1 > x4:
+                r = Px - 1
+            else:
+                l = Px + 1
+        
+        return -1
+         
