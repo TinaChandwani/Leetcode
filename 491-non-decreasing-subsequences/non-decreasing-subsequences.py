11652e13aa02dc09e80d8558fba0723187bf15bc
Tina Chandwani <tchandwa@usc.edu>
2026-05-01 13:40:25 -0700

    Time: 19 ms (61.18%) | Memory: 25 MB (82.12%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.py
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.py
@@ -2,16 +2,18 @@ class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
         def backtrack(idx,curr,res):
             if len(curr) >= 2:
-                res.add(tuple(curr))
-
+                res.append(curr[:])
+            
+            dup = set()
             
             for i in range(idx,len(nums)):
-                if len(curr) == 0 or nums[i] >= curr[-1]:
+                if (len(curr) == 0 or nums[i] >= curr[-1]) and nums[i] not in dup:
                     curr.append(nums[i])
                     backtrack(i + 1,curr,res)
                     curr.pop()
+                    dup.add(nums[i])
             
-            return list(res)
+            return res
         
-        return backtrack(0,[],set())
+        return backtrack(0,[],[])
                 
