d788db9a8144ec912e91cd2ae8fd2b797d43ade5
Tina Chandwani <tchandwa@usc.edu>
2026-06-09 17:38:04 -0700

    Time: 73 ms (8.18%) | Memory: 25.7 MB (11.93%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -3,7 +3,7 @@ class Solution:
         count = float('inf')
         memo = {}
 
-        def dp(i, j, count):
+        def dp(i, j):
             if i == len(word1):
                 return (len(word2) - j) # insertions
             
@@ -11,7 +11,7 @@ class Solution:
                 return (len(word1) - i)  # deletions
             
             if word1[i] == word2[j]:
-                return dp(i + 1, j + 1,count)
+                return dp(i + 1, j + 1)
             
             if (i,j) in memo:
                 return memo[(i,j)]
@@ -19,17 +19,17 @@ class Solution:
             # Perfom all operations
 
             # insert
-            c = 1 + dp(i, j + 1, count + 1)
+            c = 1 + dp(i, j + 1)
 
             # delete
-            c1 = 1 + dp(i + 1, j, count + 1)
+            c1 = 1 + dp(i + 1, j)
 
             # replace
-            c2 = 1 + dp(i + 1, j + 1, count + 1)
+            c2 = 1 + dp(i + 1, j + 1)
 
             ans = min(c,c1,c2)
             memo[(i,j)] = ans
 
             return ans
 
-        return dp(0, 0, 0)
+        return dp(0, 0)
