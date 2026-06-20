fe029c1d303be2e95ece3f9c12d05f8535afd24e
Tina Chandwani <tchandwa@usc.edu>
2026-06-20 16:13:07 -0700

    Time: 3 ms (52.65%) | Memory: 19.4 MB (80.76%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,36 +1,39 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        '''
+        """
         create a frequency map to store the freq of eeach characters
         Put that values into max heap
-        '''
+        """
         sDict = defaultdict(int)
         heap = []
         ans = ""
+        maxFreq = 0
 
         for i in s:
             sDict[i] += 1
-        
-        for char,freq in sDict.items():
+
+        for char, freq in sDict.items():
             ni = -1 * freq
-            heapq.heappush(heap,(ni,char))
+            heapq.heappush(heap, (ni, char))
+            maxFreq = max(maxFreq,freq)
+
+        if maxFreq > (len(s) + 1) // 2:
+            return ""
         
+        prev = None
+
         while heap:
-            first,c = heapq.heappop(heap)
-            if (-1 * first) > (len(s) + 1) // 2:
-                return ""
-            ans += c
-            first += 1
-            if heap:
-                second,c2 = heapq.heappop(heap)
-                ans += c2
-                second += 1
-                if second != 0:
-                    heapq.heappush(heap,(second,c2))
-            if first != 0:
-                heapq.heappush(heap,(first,c))
+            curr, char = heapq.heappop(heap)
+
+            ans += char
+            curr += 1
+
+            if prev:
+                heapq.heappush(heap, prev)
+
+            if (-1 * curr) > 0:
+                prev = (curr, char)
+            else:
+                prev = None
 
         return ans
-            
-
-            
