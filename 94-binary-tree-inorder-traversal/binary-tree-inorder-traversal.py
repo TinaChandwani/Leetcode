35d07b4bb1903abd2fe852054b5034a6059ed9c9
Tina Chandwani <tchandwa@usc.edu>
2026-07-10 14:35:05 -0700

    Time: 3 ms (2.10%) | Memory: 19.3 MB (26.43%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -6,18 +6,32 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        """
+        Morris Algorithm:
+        1) Take a curr node :
+            -> check if node.left exist then node.left.right and connect with the curr node (root node)
+        """
         if root is None:
             return []
-        
-        ans = []
 
-        if root.left:
-            ans.extend(self.inorderTraversal(root.left))
-        
-        ans.append(root.val)
+        curr = root
+        res = []
 
-        if root.right:
-            ans.extend(self.inorderTraversal(root.right))
-        
-        return ans
-        
+        while curr is not None:
+            if curr.left is None:
+                res.append(curr.val)
+                curr = curr.right
+            else:
+                leftchild = curr.left
+
+                while leftchild.right :
+                    leftchild = leftchild.right
+
+                leftchild.right = curr
+
+                # curr ka left ko None kar sakte ho
+                temp = curr
+                curr = curr.left
+                temp.left = None
+
+        return res
