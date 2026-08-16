05d568bae856f65ae4e76db31bed960ac3ce6c38
Tina Chandwani <tchandwa@usc.edu>
2026-08-16 14:37:43 -0700

    Time: 243 ms (58.76%) | Memory: 21.7 MB (36.05%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,24 +1,26 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m = len(grid)
         n = len(grid[0])
         q = deque()
-        count = 0
-        visit = set()
-        directions = [(0,1), (1,0), (0,-1), (-1,0)]
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        island = 0
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1':
-                    count += 1
+                if grid[i][j] == "1":
                     q.append((i,j))
-                    while q:
-                        r,c = q.popleft()
-                        visit.add((i,j))
-                        for row,col in directions:
-                            nr, nc = row + r, col + c
-                            if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == '1' and (nr,nc) not in visit:
-                                grid[nr][nc] = '0'
-                                q.append((nr,nc))
+                    while q :
+                        i1,j1 = q.pop()
+                        grid[i1][j1] = "0"
+
+                        for r,c in directions:
+                            new_r = i1 + r
+                            new_c = j1 + c
+                            if 0 <= new_r < m and 0 <= new_c < n and grid[new_r][new_c] == "1":
+                                q.append((new_r,new_c))
+
+                    island += 1
         
-        return count
+        return island
