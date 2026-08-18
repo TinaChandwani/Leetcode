9cbc7669dcd3648ea41895a0272a5c7dc2b009e4
Tina Chandwani <tchandwa@usc.edu>
2026-08-18 16:41:35 -0700

    Time: 0 ms (100.00%) | Memory: 20.4 MB (74.37%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,34 +1,30 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        '''
-        To Print the Topological Ordering
-        '''
-        indegree = [0] * numCourses
-        visit = set()
-        ans = []
         adj = defaultdict(list)
+        indegree = [0] * numCourses
+        q = deque()
+        count = 0
+        res = []
+
         for i,j in prerequisites:
             adj[j].append(i)
             indegree[i] += 1
-        print(f'adj {adj}')
-        print(f'indegree {indegree}')
-        for i in range(numCourses):
-            if i in visit:
-                continue
-            q = deque()
-            for d in range(len(indegree)):
-                if indegree[d] == 0 and d not in visit:
-                    q.append(d)
-            while q:
-                node = q.popleft()
-                visit.add(node)
-                ans.append(node)
-                for n in adj[node]:
-                    indegree[n] -= 1
-                    if indegree[n] == 0:
-                        q.append(n)
-        if len(visit) != numCourses:
-                return [] 
-        return ans
-
         
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+        
+        while q:
+            node = q.popleft()
+            res.append(node)
+            count += 1
+
+            for n in adj[node]:
+                indegree[n] -= 1
+                if indegree[n] == 0:
+                    q.append(n)
+        
+        if count == numCourses:
+            return res
+        else:
+            return []
